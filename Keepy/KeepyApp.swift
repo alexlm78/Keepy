09630f443e3349f3daaf7d537ead526b35104ea9
Alejandro Lopez Monzon <alexlm78@gmail.com>
2025-11-05 18:08:59 -0600

    Initial Commit

---
+++ Keepy/KeepyApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepyApp.swift
+//  Keepy
+//
+//  Created by Alejandro Lopez Monzon on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct KeepyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
